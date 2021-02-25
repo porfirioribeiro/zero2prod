087d9872552a996f68ca893a3c05e2510818a8bb
Porf'irio Ribeiro <porfirioribeiro@gmail.com>
2021-02-25 12:32:15 +0000

    Add sqlx

---
+++ migrations/20210223224912_create_subscriptions_table.sql
@@ -0,0 +1,9 @@
+-- Create Subscriptions Table
+CREATE TABLE subscriptions
+(
+    id            uuid        NOT NULL,
+    email         TEXT        NOT NULL UNIQUE,
+    name          TEXT        NOT NULL,
+    subscribed_at timestamptz NOT NULL,
+    PRIMARY KEY (id)
+);
