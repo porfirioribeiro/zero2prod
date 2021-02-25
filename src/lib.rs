6969e351f3273521c861a52d78c3561f50a55274
Porf'irio Ribeiro <porfirioribeiro@gmail.com>
2021-02-25 12:46:24 +0000

    Reorganize code

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,4 @@
-use std::net::TcpListener;
-
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-
+pub mod configuration;
+pub mod routes;
+pub mod startup;
 pub mod utils;
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
-    Ok(server)
-}
---
+++ src/routes/mod.rs
@@ -0,0 +1,5 @@
+pub use health_check::*;
+pub use subscriptions::*;
+
+mod health_check;
+mod subscriptions;
