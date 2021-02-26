c6d76de8bffadffe2f8ce45a223ecab9eb348e3c
Porf'irio Ribeiro <porfirioribeiro@gmail.com>
2021-02-26 16:20:38 +0000

    dockerize

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
 
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -12,11 +12,17 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_with(configuration.database.with_db())
         .await
         .expect("Failed to connect to Postgres.");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
 }
